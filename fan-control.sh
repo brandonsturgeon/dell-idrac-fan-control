1a0817edd2ae31268f0683d0d6c684f753d6d58e
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-10-28 09:30:43 -0700

    Update

--- fan-control.sh
+++ fan-control.sh
@@ -1,30 +1,43 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-if [ ! -f idrac-creds.sh ]; then
-        echo "IDRAC credentials file not found...aborting"
-        exit 0
-    else
-        source idrac-creds.sh
-fi
-
+SCRIPT_DIR=/home/brandon/scripts/fanspeed
+LOG_FILE=$SCRIPT_DIR/fanspeed.log
 DATE=$(date +%Y-%m-%d-%H:%M:%S)
 SET_SPEED="16" # This is number 16 in percentage of fan speed. 16% speed works well for me being quiet enough. See README for more info.
 SENSOR_NAME="Temp"
 MAX_TEMP="37"
 
+log () {
+    echo "[$DATE] $1" >> $LOG_FILE
+}
+
+log ""
+echo $(date) >> $LOG_FILE
+
+if [ ! -f $SCRIPT_DIR/idrac-creds.sh ]; then
+        echo "IDRAC credentials file not found...aborting"
+        exit 0
+    else
+        source $SCRIPT_DIR/idrac-creds.sh
+fi
+
+
 DEC_HEX=$(printf '%x\n' $SET_SPEED)
 
-TEMP=$(ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_UNAME -P $IDRAC_PASSWD sdr type temperature | grep -m 1 "$SENSOR_NAME" | awk '{ print $9 }')
+TEMP=$(ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_UNAME -P $IDRAC_PASSWD sdr type temperature)
+log "$TEMP"
+TEMP=$(echo $TEMP | grep -m 1 "$SENSOR_NAME")
+TEMP=$(echo $TEMP | awk '{ print $21 }')
 
-echo "[$DATE] $IDRAC_IP: temp is $TEMP degrees C"
+log "$IDRAC_IP: temp is $TEMP degrees C"
 if [[ $TEMP > $MAX_TEMP ]]
   then
-    printf "[$DATE] Getting hot in here - enabling dynamic mode"
+    log "Getting hot in here - enabling dynamic mode"
     ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_UNAME -P $IDRAC_PASSWD raw 0x30 0x30 0x01 0x01
   else
-    echo "[$DATE] Temps are less than $MAX_TEMP degrees C ...setting fan speed to $SET_SPEED%"
+    log "Temps are less than $MAX_TEMP degrees C ...setting fan speed to $SET_SPEED%"
     ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_UNAME -P $IDRAC_PASSWD raw 0x30 0x30 0x01 0x00
     ipmitool -I lanplus -H $IDRAC_IP -U $IDRAC_UNAME -P $IDRAC_PASSWD raw 0x30 0x30 0x02 0xff 0x$DEC_HEX
     sleep 1
-    echo "[Setting applied]"
+    log "Setting applied"
 fi
